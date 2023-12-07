206482d77049cffed3192083dcd191b413bf75d0
entreri514 <148565529+entreri514@users.noreply.github.com>
2023-12-07 14:34:33 -0600

    Added movieList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-
+import MovieList from "./components/MovieList/MovieList";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Header />
+      <MovieList />
     </div>
   );
 }
